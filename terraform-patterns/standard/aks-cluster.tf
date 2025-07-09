1d6d0924ccd212a9e5db0ee5d7df020eba5dcea0
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-07-08 20:31:25 -0400

    k8 manifest fix

--- terraform-patterns/standard/aks-cluster.tf
+++ terraform-patterns/standard/aks-cluster.tf
@@ -26,3 +26,39 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+# Data source to find the Network Security Group created by AKS
+data "azurerm_resources" "aks_nsg" {
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+  type                = "Microsoft.Network/networkSecurityGroups"
+}
+
+# Add a rule to the AKS-created NSG to allow HTTPS traffic
+resource "azurerm_network_security_rule" "allow_https_inbound" {
+  name                        = "AllowHTTPSInboundFromInternet"
+  priority                    = 101 # Lower number = higher priority
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_kubernetes_cluster.aks.node_resource_group
+  network_security_group_name = data.azurerm_resources.aks_nsg.resources[0].name
+}
+
+# Add a rule to the AKS-created NSG to allow HTTP traffic
+resource "azurerm_network_security_rule" "allow_http_inbound" {
+  name                        = "AllowHTTPInboundFromInternet"
+  priority                    = 102 # Runs after the HTTPS rule
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_kubernetes_cluster.aks.node_resource_group
+  network_security_group_name = data.azurerm_resources.aks_nsg.resources[0].name
+}
+
--- terraform-patterns/standard/helm-charts/tool-install.tf
+++ terraform-patterns/standard/helm-charts/tool-install.tf
@@ -65,3 +65,17 @@ resource "helm_release" "ingress" {
     file("${path.module}/nginx-ingress/values.yaml")
   ]
 }
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "1.14.3"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
